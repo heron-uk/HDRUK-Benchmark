2616c06fe4096bd908cef3c539a8599cdc6e267f
catalamarti <marticatalasabate@gmail.com>
2024-11-20 17:00:29 +0000

    add commets to codeToRun

--- Benchmark/CodeToRun.R
+++ Benchmark/CodeToRun.R
@@ -1,23 +1,31 @@
-db_name <- "..."
+# to restore renv environment
+renv::restore()
 
+# data base name, use an acronym to identify the database (e.g "CPRD GOLD")
+dbName <- "..."
+
+# create a DBI connection to your database
 con <- DBI::dbConnect("...")
 
+# schema in the database that contains OMOP CDM standard tables
 cdmSchema <- "..."
+
+# schema in the database where you have writing permissions
 writeSchema <- "..."
 
+# created tables will start with this prefix
 prefix <- "..."
 
+# minimum cell counts used for suppression
+minCellCount <- 5
 
+# to create the cdm object
 cdm <- CDMConnector::cdmFromCon(
   con = con,
   cdmSchema = cdmSchema,
-  writeSchema = c(
-    schema = writeSchema,
-    prefix = prefix
-  )
+  writeSchema = c(schema = writeSchema, prefix = prefix),
+  cdmName = dbName
 )
 
-
-
-
+# run study code
 source("R/RunBenchmark.R")
